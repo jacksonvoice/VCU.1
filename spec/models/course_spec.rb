fd93f44c679bb0e3e254fe45089bcb029e67ad14
Daniel Jackson <jacksonvoice@yahoo.com>
2014-09-18 11:26:14 -0700

    got the test working for course name validations, and creating a course

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,4 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Course, :type => :model do
+
+	it "should create a valid course" do
+		course = Course.new(name: 'name1')
+		expect(course).to be_valid
+	end
+
+	it "should be invalid with no name" do
+		course = Course.new(name: '')
+		expect(course).to be_invalid
+	end
+
+	it { should have_many(:videos).through(:sections) }
+	it { should have_many(:sections) }
+
+	it "should be invalid with a duplicate name" do 
+		Course.create(name: 'name1')
+		course = Course.new(name: 'name1')
+		expect(course).to have(1).errors_on(:name)
+	end
+
+
 end
